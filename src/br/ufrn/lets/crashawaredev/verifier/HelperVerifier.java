d77858c261262cc3cccdf682b46b7f1b9c169f1b
leandro <leandro@leandro>
2018-10-30 21:57:32 -0300

    Continuação do HelperVerifier

--- src/br/ufrn/lets/crashawaredev/verifier/HelperVerifier.java
+++ src/br/ufrn/lets/crashawaredev/verifier/HelperVerifier.java
@@ -8,6 +8,7 @@ import java.util.regex.Pattern;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.runtime.ILog;
 import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
+import org.eclipse.jdt.internal.ui.text.correction.VarargsWarningsSubProcessor;
 
 import br.ufrn.lets.exceptionexpert.models.ASTExceptionRepresentation;
 import br.ufrn.lets.exceptionexpert.models.MethodRepresentation;
@@ -34,22 +35,33 @@ public class HelperVerifier extends PatternVerifier {
 				
 				String methodBodyString = method.getMethodDeclaration().getBody().statements().toString();
 				
+				List<SingleVariableDeclaration> varsToValidate = new ArrayList<>();
+				
 				for(Object param : method.getMethodDeclaration().parameters()) {
 					
 					SingleVariableDeclaration var = (SingleVariableDeclaration) param;
 					
 					if(!validateArgument(var, methodBodyString)) {
-						ReturnMessage rm = new ReturnMessage();
-						rm.setMessage("Valide os argumentos deste método!");
-						rm.setLineNumber(astRep.getAstRoot().getLineNumber(method.getMethodDeclaration().getStartPosition()));
-						rm.setMarkerSeverity(IMarker.SEVERITY_WARNING);
-						messages.add(rm);
+						varsToValidate.add(var);
 					}
-					
 				}
 				
+				if(!varsToValidate.isEmpty()) {
+					
+					String varStr = "(";
+					for(SingleVariableDeclaration svar : varsToValidate) {
+						varStr += svar.toString() + ", ";
+					}
+					varStr = varStr.substring(0, varStr.length()-2) + ")";	
+										
+					ReturnMessage rm = new ReturnMessage();
+					rm.setMessage("Os argumentos deste método devem ser validados para evitar potenciais null pointers."
+							+ "\nValide cada um dos seguintes argumentos antes de referenciá-los: " + varStr);
+					rm.setLineNumber(astRep.getAstRoot().getLineNumber(method.getMethodDeclaration().getStartPosition()));
+					rm.setMarkerSeverity(IMarker.SEVERITY_WARNING);
+					messages.add(rm);
+				}
 			}
-			
 		}
 		
 		return messages;
@@ -65,13 +77,13 @@ public class HelperVerifier extends PatternVerifier {
 		String varName = var.getName().toString();
 		
 		// Valida se existe checkagem da variável igual ou diferente de null
-		Pattern pattern = Pattern.compile(varName + "\\s*==|!=\\s*null");
+		Pattern pattern = Pattern.compile(varName + "\\s*(==|!=)\\s*null");
 		Matcher matcher = pattern.matcher(methodBody);
 		if(matcher.find())
 			return true;
 		
 		// Valida se foi usado o ValidatorUtil para checkar a variável
-		pattern = Pattern.compile("ValidatorUtil\\.is(Not)Empty\\(\\s*" + varName);
+		pattern = Pattern.compile("ValidatorUtil\\.is(Not)*Empty\\(\\s*" + varName);
 		matcher = pattern.matcher(methodBody);
 		if(matcher.find())
 			return true;
